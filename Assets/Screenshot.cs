700e1ee70872626f1bfb6c33bf0beb1d434866d1
jbarba93 <jbarba@hawk.iit.edu>
2017-01-24 11:12:21 -0600

    170124_InClass

---
+++ Assets/Screenshot.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Screenshot : MonoBehaviour {
+
+    // Use this for initialization
+    void Start()
+    {
+        Debug.Log("Hello!");
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+
+    void OnMouseDown()
+    {
+        Debug.Log("Hello!");
+        //Application.CaptureScreenshot("Screenshot.png");
+    }
+}
---
+++ Assets/Trigger.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Trigger : MonoBehaviour
+{
+
+    Renderer r;
+
+	// Use this for initialization
+	void Start ()
+    {
+        r = GetComponent<Renderer>();
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+
+    void OnTriggerEnter()
+    {
+        Debug.Log("Enter");
+        r.material.color(new Color(255, 0, 0));
+    }
+}
